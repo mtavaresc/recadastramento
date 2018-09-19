6a94ba824d87870e4e53daa02a64f7df20396e33
Marcelo Tavares <marcelo.t498@gmail.com>
2018-09-19 12:46:02 -0300

    Modeling;

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request
+from base import app, db
+from model import Pegaso, Trabalhador
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///alece.db'
-
-db = SQLAlchemy(app)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# Creating new database of model "Worker"
+db.create_all(bind="out")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/<int:matricula>", methods=["GET", "POST"])
+def recadastrar(matricula):
+    pegaso = Pegaso.query.filter_by(matricula=matricula).first()
+
+    if request.method == "POST":
+        cpf = request.form.get("cpf")
+        nis = request.form.get("nis")
+        nome = request.form.get("nome")
+        sexo = request.form.get("sexo")
+        raca = request.form.get("raca")
+        estciv = request.form.get("estciv")
+        dtnascto = request.form.get("dtnascto")
+
+        c = Trabalhador(cpf, nis, nome, sexo, raca, estciv, dtnascto)
+        db.session.merge(c)
+        db.session.commit()
+
+        return render_template("submit.html")
+    return render_template("index.html", pegaso=pegaso)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,9 +1,67 @@
-from app import db
+from base import db
 
 
-class Colaborador(db.Model):
-    __tablename__ = 'colaborador'
+class Pegaso(db.Model):
+    __tablename__ = "pegaso"
+    __bind_by__ = "in"
 
-    cpf_trab = db.Column('cpfTrab', db.String(11))
-    nis_trab = db.Column('nisTrab', db.String(11))
-    nm_trab = db.Column('nmTrab', db.String(70))
+    matricula = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(11))
+    nis = db.Column(db.String(11))
+    dtnascto = db.Column(db.String(11))
+
+
+class Trabalhador(db.Model):
+    __tablename__ = "trabalhador"
+    __bind_key__ = "out"
+
+    # Trabalhador
+    cpfTrab = db.Column(db.String(11))
+    nisTrab = db.Column(db.String(11))
+    nmTrab = db.Column(db.String(70))
+    sexo = db.Column(db.String(1))
+    racaCor = db.Column(db.Integer)
+    estCiv = db.Column(db.Integer)
+    grauInstr = db.Column(db.String(2))
+    indPriEmpr = db.Column(db.String(1))
+    nmSoc = db.Column(db.String(70))
+    # Nascimento
+    dtNascto = db.Column(db.Date)
+    codMunic = db.Column(db.Integer)
+    uf = db.Column(db.String(2))
+    paisNascto = db.Column(db.String(3))
+    paisNac = db.Column(db.String(3))
+    nmMae = db.Column(db.String(70))
+    nmPai = db.Column(db.String(70))
+    # CTPS
+    nrCtpts = db.Column(db.String(11))
+    serieCtps = db.Column(db.String(5))
+    ufCtps = db.Column(db.String(2))
+    # RIC
+    nrRic = db.Column(db.String(14))
+    ric_orgaoEmissor = db.Column(db.String(20))
+    ric_dtExped = db.Column(db.Date)
+    # RG
+    nrRg = db.Column(db.String(14))
+    rg_orgaoEmissor = db.Column(db.String(20))
+    rg_dtExped = db.Column(db.Date)
+    # RNE
+    nrRne = db.Column(db.String(14))
+    rne_orgaoEmissor = db.Column(db.String(20))
+    rne_dtExped = db.Column(db.Date)
+    # OC
+    nrOc = db.Column(db.String(14))
+    oc_orgaoEmissor = db.Column(db.String(20))
+    oc_dtExped = db.Column(db.Date)
+    oc_dtValid = db.Column(db.Date)
+
+    db.PrimaryKeyConstraint(cpfTrab, nisTrab, dtNascto)
+
+    def __init__(self, cpf, nis, nome, sexo, raca, est_civ, dtnascto):
+        self.cpf = cpf
+        self.nis = nis
+        self.nome = nome
+        self.sexo = sexo
+        self.raca = raca
+        self.est_civ = est_civ
+        self.dtNascto = dtnascto
