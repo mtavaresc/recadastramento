482f559708cf1211173197b16a929b5d31af8672
Marcelo Tavares <marcelo.t498@gmail.com>
2018-09-25 11:22:16 -0300

    Fix production environment;

--- app.py
+++ app.py
@@ -15,103 +15,116 @@ def hello_world():
 
 @app.route("/<matricula>", methods=["GET", "POST"])
 def recadastrar(matricula):
+    # Campos preenchidos através da matricula do Pegaso
     pegaso = db.session.execute(
         "SELECT nome, cpf, pispasep, TO_CHAR(dtnasc, 'YYYY-MM-DD') as dtnasc "
         "FROM zeus.tv_cadastro WHERE matr = '{}'".format(matricula)).first()
 
+    # Selecionando paises
+    # cod_paises = [row.codigo for row in Paises.query.all()]
+    # nome_paises = [row.nome for row in Paises.query.all()]
+    # paises = dict(zip(cod_paises, nome_paises))
+    # Selecionando estados
+    # uf_estados = [row.uf for row in Estados.query.all()]
+    # nome_estados = [row.nome for row in Estados.query.order_by(Estados.nome).all()]
+    # estados = dict(zip(uf_estados, nome_estados))
+    # Selecionando municipios
+    # cod_municipio = [row.codigo for row in Municipios.query.all()]
+    # nome_municipio = [row.nome for row in Municipios.query.order_by(Municipios.nome).all()]
+    # municipios = dict(zip(cod_municipio, nome_municipio))
+    # Selecionando tipos logradouro
+    # cod_tl = [row.codigo for row in TiposLogradouro.query.all()]
+    # nome_tl = [row.nome for row in TiposLogradouro.query.order_by(TiposLogradouro.nome).all()]
+    # tipos_logradouro = dict(zip(cod_tl, nome_tl))
+    # Selecionando bairros
+    # bairros = [row.nome for row in db.session.query(Bairros.nome).distinct().order_by(Bairros.nome).all()]
+
     if request.method == "POST":
         # Trabalhador
-        cpf_trab = request.form.get("")
-        nis_trab = request.form.get("")
-        nm_trab = request.form.get("")
-        sexo = request.form.get("")
-        raca_cor = request.form.get("")
-        est_civ = request.form.get("")
-        grau_instr = request.form.get("")
-        ind_pri_empr = request.form.get("")
-        nm_soc = request.form.get("")
+        cpf_trab = request.form.get("cpfTrab")
+        cpf_trab = "".join(c for c in str(cpf_trab) if c not in ".-")
+        nis_trab = request.form.get("nisTrab")
+        nis_trab = "".join(c for c in str(nis_trab) if c not in ".-")
+        nm_trab = request.form.get("nmTrab")
+        sexo = request.form.get("sexo")
+        raca_cor = request.form.get("racaCor")
+        est_civ = request.form.get("estCiv")
+        grau_instr = request.form.get("grauInstr")
+        ind_pri_empr = request.form.get("indPriEmpr")
+        nm_soc = request.form.get("nmSoc")
         # Nascimento
-        dt_nascto = request.form.get("")
-        cod_munic = request.form.get("")
-        uf = request.form.get("")
-        pais_nascto = request.form.get("")
-        pais_nac = request.form.get("")
-        nm_mae = request.form.get("")
-        nm_pai = request.form.get("")
+        dt_nascto = request.form.get("dtNascto")
+        cod_munic = request.form.get("codMunic")
+        uf = request.form.get("uf")
+        pais_nascto = request.form.get("paisNascto")
+        pais_nac = request.form.get("paisNac")
+        nm_mae = request.form.get("nmMae")
+        nm_pai = request.form.get("nmPai")
         # CTPS
-        nr_ctps = request.form.get("")
-        serie_ctps = request.form.get("")
-        uf_ctps = request.form.get("")
-        # RIC
-        # nr_ric = request.form.get("")
-        # ric_orgao_emissor = request.form.get("")
-        # ric_dt_exped = request.form.get("")
+        nr_ctps = request.form.get("nrCtps")
+        serie_ctps = request.form.get("serieCtps")
+        uf_ctps = request.form.get("ufCtps")
         # RG
-        nr_rg = request.form.get("")
-        rg_orgao_emissor = request.form.get("")
-        rg_dt_exped = request.form.get("")
-        # RNE
-        # nr_rne = request.form.get("")
-        # rne_orgao_emissor = request.form.get("")
-        # rne_dt_exped = request.form.get("")
+        nr_rg = request.form.get("nrRg")
+        rg_orgao_emissor = request.form.get("rg_orgaoEmissor")
+        rg_dt_exped = request.form.get("rg_dtExped")
         # OC
-        nr_oc = request.form.get("")
-        oc_orgao_emissor = request.form.get("")
-        oc_dt_exped = request.form.get("")
-        oc_dt_valid = request.form.get("")
+        nr_oc = request.form.get("nrOc")
+        oc_orgao_emissor = request.form.get("oc_orgaoEmissor")
+        oc_dt_exped = None if request.form.get("oc_dtExped") is "" else request.form.get("oc_dtExped")
+        oc_dt_valid = None if request.form.get("oc_dtValid") is "" else request.form.get("oc_dtValid")
         # CNH
-        nr_reg_cnh = request.form.get("")
-        cnh_dt_exped = request.form.get("")
-        uf_cnh = request.form.get("")
-        cnh_dt_valid = request.form.get("")
-        dt_pri_hab = request.form.get("")
-        categoria_cnh = request.form.get("")
+        nr_reg_cnh = request.form.get("nrRegCnh")
+        cnh_dt_exped = None if request.form.get("cnh_dtExped") is "" else request.form.get("cnh_dtExped")
+        uf_cnh = request.form.get("ufCnh")
+        cnh_dt_valid = None if request.form.get("cnh_dtValid") is "" else request.form.get("cnh_dtValid")
+        dt_pri_hab = None if request.form.get("dtPriHab") is "" else request.form.get("dtPriHab")
+        categoria_cnh = request.form.get("categoriaCnh")
         # Endereco - Brasil
-        tp_lograd = request.form.get("")
-        dsc_lograd = request.form.get("")
-        nr_lograd = request.form.get("")
-        complemento = request.form.get("")
-        bairro = request.form.get("")
-        cep = request.form.get("")
-        end_cod_munic = request.form.get("")
-        end_uf = request.form.get("")
+        tp_lograd = request.form.get("tpLograd")
+        dsc_lograd = request.form.get("dscLograd")
+        nr_lograd = request.form.get("nrLograd")
+        complemento = request.form.get("complemento")
+        bairro = request.form.get("bairro")
+        cep = request.form.get("cep")
+        cep = "".join(c for c in str(cep) if c not in "-")
+        end_cod_munic = request.form.get("end_codMunic")
+        end_uf = request.form.get("end_uf")
         # Endereco - Exterior
-        pais_resid = request.form.get("")
-        ext_dsc_lograd = request.form.get("")
-        ext_nr_lograd = request.form.get("")
-        ext_complemento = request.form.get("")
-        ext_bairro = request.form.get("")
-        nm_cid = request.form.get("")
-        cod_postal = request.form.get("")
-        # Estrangeiro
-        # dt_chegada = request.form.get("")
-        # class_trab_estran = request.form.get("")
-        # casado_br = request.form.get("")
-        # filhos_br = request.form.get("")
+        pais_resid = request.form.get("paisResid")
+        ext_dsc_lograd = request.form.get("ext_dscLograd")
+        ext_nr_lograd = request.form.get("ext_nrLograd")
+        ext_complemento = request.form.get("ext_complemento")
+        ext_bairro = request.form.get("ext_bairro")
+        nm_cid = request.form.get("nmCid")
+        cod_postal = request.form.get("codPostal")
         # Info Deficiencia
-        def_fisica = request.form.get("")
-        def_visual = request.form.get("")
-        def_auditiva = request.form.get("")
-        def_mental = request.form.get("")
-        def_intelectual = request.form.get("")
-        def_readap = request.form.get("")
-        info_cota = request.form.get("")
-        observacao = request.form.get("")
+        def_fisica = request.form.get("defFisica")
+        def_visual = request.form.get("defVisual")
+        def_auditiva = request.form.get("defAuditiva")
+        def_mental = request.form.get("defMental")
+        def_intelectual = request.form.get("defIntelectual")
+        def_readap = request.form.get("defReadap")
+        info_cota = request.form.get("infoCota")
+        observacao = request.form.get("observacao")
         # Dependete
-        tp_dep = request.form.get("")
-        nm_dep = request.form.get("")
-        dep_dt_nascto = request.form.get("")
-        cpf_dep = request.form.get("")
-        dep_irrf = request.form.get("")
-        dep_sf = request.form.get("")
-        inc_trab = request.form.get("")
+        tp_dep = request.form.get("tpDep")
+        nm_dep = request.form.get("nmDep")
+        dep_dt_nascto = None if request.form.get("dep_dtNascto") is "" else request.form.get("dep_dtNascto")
+        cpf_dep = request.form.get("cpfDep")
+        cpf_dep = "".join(c for c in str(cpf_dep) if c not in ".-")
+        dep_irrf = request.form.get("depIRRF")
+        dep_sf = request.form.get("depSF")
+        inc_trab = request.form.get("incTrab")
         # Aposentadoria
-        trab_aposent = request.form.get("")
+        trab_aposent = request.form.get("trabAposent")
         # Contato
-        fone_princ = request.form.get("")
-        fone_alternat = request.form.get("")
-        email_princ = request.form.get("")
-        email_alternat = request.form.get("")
+        fone_princ = request.form.get("fonePrinc")
+        fone_princ = "".join(c for c in str(fone_princ) if c not in "()- ")
+        fone_alternat = request.form.get("foneAlternat")
+        fone_alternat = "".join(c for c in str(fone_alternat) if c not in "()- ")
+        email_princ = request.form.get("emailPrinc")
+        email_alternat = request.form.get("emailAlternat")
 
         c = Trabalhador(cpf_trab, nis_trab, nm_trab, sexo, raca_cor, est_civ, grau_instr, ind_pri_empr, nm_soc,
                         dt_nascto, cod_munic, uf, pais_nascto, pais_nac, nm_mae, nm_pai, nr_ctps, serie_ctps, uf_ctps,
@@ -127,7 +140,8 @@ def recadastrar(matricula):
 
         return render_template("submit.html")
 
-    return render_template("index.html", pegaso=pegaso)
+    # return render_template("index.html", pegaso=pegaso, paises=paises, estados=estados, municipios=municipios,
+    #                        tl=tipos_logradouro, bairros=bairros)
 
 
 if __name__ == "__main__":
