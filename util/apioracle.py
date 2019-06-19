3720c59786efd8566d72b5d754f53038a15eb1b7
Marcelo Tavares <marcelo.t498@gmail.com>
2019-06-19 17:59:48 -0300

    Add generate act automatic

--- util/apioracle.py
+++ util/apioracle.py
@@ -7,8 +7,15 @@ class Acts:
         self.base = 'http://apioracle.al.ce.gov.br/'
         self.key = '$2y$14$mssekBgJ/dhLafs6/H8xpufALWgvpujtWz2m/DNbjs9PhM2KeMlD2'
 
-    def create(self):
-        pass
+    def create(self, register):
+        url = '{base}/gera_numero_ato.php?matr={register}&token={key}' \
+            .format(base=self.base, register=register, key=self.key)
+        response = requests.get(url)
+
+        if response.text == '1':
+            return True
+        else:
+            return False, response.text
 
     def list(self):
         url = '{base}/atos_lista.php?token={key}'.format(base=self.base, key=self.key)
@@ -33,15 +40,21 @@ class Acts:
 
 
 if __name__ == '__main__':
-    acts = Acts().list()
-    list_of_acts = []
-    # Registrations of DRH people
-    registrations = ['025164', '030108', '004018', '002968']
+    # Create Act
+    create = Acts().create(register='025164')
 
-    if acts['code']:
-        for act in acts['result']:
-            if act['ATO_MATR'] in registrations:
-                print(dumps(act, indent=4))
-                list_of_acts.append('{num}/{ano}'.format(num=act['ATO_NUM'].zfill(4), ano=act['ATO_ANO']))
+    if create:
+        # List Acts
+        acts = Acts().list()
+        list_of_acts = []
+        # Registrations of DRH people
+        registrations = ['025164', '030108', '004018', '002968']
 
-        print('\n{} ({})'.format(list_of_acts, len(list_of_acts)))
+        if acts['code']:
+            for act in acts['result']:
+                if act['ATO_MATR'] in registrations:
+                    print(dumps(act, indent=4))
+                    list_of_acts.append('{num}/{ano}'.format(num=act['ATO_NUM'].zfill(4), ano=act['ATO_ANO']))
+
+            print('\n{} ({})'.format(list_of_acts, len(list_of_acts)))
+            print(max(list_of_acts))
